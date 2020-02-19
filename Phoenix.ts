eabc7eb3040d6e1a3ff652599e9c71c361f3c5c7
Lucas Souza <lukeeifsp@hotmail.com>
2020-02-19 14:16:49 -0300

    Arrumando métodos estáticos da main

--- Phoenix.ts
+++ Phoenix.ts
@@ -1,26 +1,69 @@
 import IConfig from './IConfig';
 import { Client } from 'discord.js';
 import CommandManager from './managers/CommandManager';
+import EventManager from './managers/EventManager';
+import DatabaseManager from './managers/DatabaseManager';
+import ServerManager from './managers/ServerManager';
 
-export default class Phoenix extends Client{
-    private configuration: IConfig;
-    private commandManager = new CommandManager();
-    constructor(){
-        super();
-        this.configuration = null;
-        this.commandManager.init();
-    }
+export default class Phoenix {
+    private static configuration: IConfig = require('../config.json') as IConfig;
+    private static commandManager = new CommandManager();
+    private static eventManager = new EventManager();
+    private static databaseManager = new DatabaseManager();
+    private static serverManager = new ServerManager();
+    private static client: Client = new Client({
+        disableEveryone: true,
+        disabledEvents: [
+            "GUILD_SYNC",
+            "CHANNEL_PINS_UPDATE",
+            "USER_NOTE_UPDATE",
+            "RELATIONSHIP_ADD",
+            "RELATIONSHIP_REMOVE",
+            "USER_SETTINGS_UPDATE",
+            "VOICE_STATE_UPDATE",
+            "VOICE_SERVER_UPDATE",
+            "TYPING_START",
+            "PRESENCE_UPDATE"
+        ],
+        messageCacheLifetime: 120,
+        messageSweepInterval: 480
+    });
+
     public init() {
-        super.login(this.configuration.token).then(() => console.log('Logged on discord!')).catch(console.error);
+        Phoenix.getCommandManager().init();
+        Phoenix.getEventManager().init();
+        Phoenix.getDatabaseManager().init();
+        Phoenix.getServerManager().init();
+        Phoenix.client.login(Phoenix.getConfig().token).then(() => console.log('Logged on discord!')).catch(console.error);
+    }
+    
+    public destroy() {
+        Phoenix.getCommandManager().destroy();
     }
 
-    static get configuration(): IConfig{
-        return this.configuration;
+    public static getClient() {
+        return this.client;
     }
-
-    static get commandManager(): CommandManager{
+  
+    public static getEventManager() {
+        return this.eventManager;
+    }
+  
+    public static getCommandManager() {
         return this.commandManager;
+    }
+  
+    public static getDatabaseManager() {
+        return this.databaseManager;
+    }
+  
+    public static getServerManager() {
+        return this.serverManager;
+    }
+
+    public static getConfig() {
+        return this.configuration;
     }
 }
 
-new Phoenix();
+new Phoenix().init();
