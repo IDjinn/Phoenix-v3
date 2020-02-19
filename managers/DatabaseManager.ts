714e297c1c123ca8551e4e0605557397dc088c34
Lucas Souza <lukeeifsp@hotmail.com>
2020-02-19 14:20:14 -0300

    Fix conexão mongoose

--- managers/DatabaseManager.ts
+++ managers/DatabaseManager.ts
@@ -1,13 +1,13 @@
 import AbstractManager from "../structures/AbstractManager";
-import mongoose from 'mongoose';
 import Phoenix from "../Phoenix";
+const mongoose = require('mongoose');
 
 export default class DatabaseManager extends AbstractManager{
     constructor(){
         super('DatabaseManager');
     }
     public init() {
-        const {link, user, password} = Phoenix.configuration.database;
+        const {link, user, password} = Phoenix.getConfig().database;
         mongoose.connect(link.replace('%user%', user).replace('%password%',password)).then(() => console.log('Database logged'))
         .catch(console.error);
     }   
