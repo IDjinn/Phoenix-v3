8e8f140a70a89adf914ebcd13fc1daa3b4381e3e
Lucas Souza <lukeeifsp@hotmail.com>
2020-02-26 16:58:28 -0300

    Welcome module agora tem embed padrão caso não haja um costum #3

--- modules/WelcomeModule.ts
+++ modules/WelcomeModule.ts
@@ -1,6 +1,7 @@
 import AbstractModule from "../structures/AbstractModule";
 import Server from "../structures/Server";
 import { GuildMember, TextChannel, Collection } from "discord.js";
+import { EmbedWithTitle } from "../util/EmbedFactory";
 
 export default class WelcomeModule extends AbstractModule{
     public config: IWelcome;
@@ -19,8 +20,12 @@ export default class WelcomeModule extends AbstractModule{
     public onMemberJoin(member: GuildMember) {
         if (this.config.join.enabled) {
             let channel = member.guild.channels.get(this.config.join.channel);
-            if (channel instanceof TextChannel)
-                channel.send(this.config.join.embed).catch();
+            if (channel instanceof TextChannel) {
+                if (this.config.join.embed)
+                    channel.send(this.config.join.embed).catch();
+                else
+                    channel.send(EmbedWithTitle('User Joined', `The user ${member.toString()} joined in the ${member.guild.name} server.`)).catch();
+            }
             if (this.config.autorole.enabled)
                 member.addRoles(this.config.autorole.roles).catch();
         }
@@ -30,8 +35,12 @@ export default class WelcomeModule extends AbstractModule{
     public onMemberLeave(member: GuildMember) {
         if (this.config.leave.enabled) {
             let channel = member.guild.channels.get(this.config.leave.channel);
-            if (channel instanceof TextChannel)
-                channel.send(this.config.leave.embed).catch();
+            if (channel instanceof TextChannel) {
+                if (this.config.leave.embed)
+                    channel.send(this.config.leave.embed).catch();
+                else
+                    channel.send(EmbedWithTitle('User Leave', `The user ${member.toString()} leaved in the ${member.guild.name} server.`)).catch();
+            }
         }
         this.doorLog.set(Date.now().toString(), member.id);
     }
