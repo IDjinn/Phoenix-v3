6a2cbf79084aa139fbfa4b22376064d5cae8b13f
Lucas Souza <lukeeifsp@hotmail.com>
2020-02-25 23:24:47 -0300

    Owners agora ficam compilados na Constants

--- util/Constants.ts
+++ util/Constants.ts
@@ -1,3 +1,4 @@
 export default class Constants {
     public static getXpFromLevel = (nivel: number) => Math.floor(((nivel / 0.2) * (nivel / 0.3)) * Math.PI);
+    public static ownersList = ['376460601909706773'];
 }
