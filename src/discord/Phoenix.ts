0af9a7bf609c149fa63e3e318eaac4a3a1b21733
Luke <lukeeifsp@gmail.com>
2020-03-10 21:10:56 +0000

    DatabaseManager deletado, passado para o main do bot

--- src/discord/Phoenix.ts
+++ src/discord/Phoenix.ts
@@ -2,16 +2,15 @@ import IConfig from '../../IConfig';
 import { Client } from 'discord.js';
 import CommandManager from './managers/CommandManager';
 import EventManager from './managers/EventManager';
-import DatabaseManager from './managers/DatabaseManager';
 import ServerManager from './managers/ServerManager';
 import PhoenixUserManager from './managers/PhoenixUserManager';
 import TextManager from './managers/TextManager';
+import { connect as mongooseConnect, disconnect as mongooseDisconnect } from 'mongoose';
 
 export default class Phoenix {
     private static configuration: IConfig = require('../../../config.json') as IConfig;
     private static commandManager = new CommandManager();
     private static eventManager = new EventManager();
-    private static databaseManager = new DatabaseManager();
     private static serverManager = new ServerManager();
     private static phoenixUserManager = new PhoenixUserManager();
     private static textManager = new TextManager();
@@ -35,14 +34,19 @@ export default class Phoenix {
     });
 
     public async init() {
-        Phoenix.getDatabaseManager().init();
-        Phoenix.getServerManager().init();
-        Phoenix.getPhoenixUserManager().init();
-        Phoenix.getEventManager().init();
-        Phoenix.getCommandManager().init();
-        Phoenix.getTextManager().init();
-        await Phoenix.getClient().login(Phoenix.getConfig().token).then(() => console.log('Logged on discord!')).catch(console.error);
-        Phoenix.INVITE = await Phoenix.getClient().generateInvite('ADMINISTRATOR');
+        const { link, user, password } = Phoenix.getConfig().database;
+        try {
+            await mongooseConnect(link.replace('%user%', user).replace('%password%', password), { useNewUrlParser: true, useUnifiedTopology: true });
+            console.log('Database ready');
+            Phoenix.getServerManager().init();
+            Phoenix.getPhoenixUserManager().init();
+            Phoenix.getEventManager().init();
+            Phoenix.getCommandManager().init();
+            await Phoenix.getClient().login(Phoenix.getConfig().token).then(() => console.log('Logged on discord!')).catch(console.error);
+        }
+        catch (error) {
+            console.error(`Mongoose doesn't connected, Phoenix is shuttedown. Error: ${error}`);
+        }
     }
     
     public static destroy() {
@@ -50,8 +54,7 @@ export default class Phoenix {
         Phoenix.getPhoenixUserManager().destroy();
         Phoenix.getEventManager().destroy();
         Phoenix.getCommandManager().destroy();
-        Phoenix.getTextManager().destroy();
-        Phoenix.getDatabaseManager().destroy();
+        mongooseDisconnect();
     }
 
     public static getClient() {
@@ -66,10 +69,6 @@ export default class Phoenix {
         return this.commandManager;
     }
   
-    public static getDatabaseManager() {
-        return this.databaseManager;
-    }
-  
     public static getServerManager() {
         return this.serverManager;
     }
@@ -78,7 +77,7 @@ export default class Phoenix {
         return this.phoenixUserManager;
     }
 
-    public static getTextManager() {
+    public static getTextManager(){
         return this.textManager;
     }
 
