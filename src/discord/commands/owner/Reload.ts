f170cee0c32cd031c4ae680436e1f7d721811571
IDjinn <lukeeifsp@gmail.com>
2020-04-18 17:10:50 -0300

    Subcomandos para o Reload

--- src/discord/commands/owner/Reload.ts
+++ src/discord/commands/owner/Reload.ts
@@ -1,5 +1,6 @@
 import AbstractCommand, { ICommandParameters } from "../../structures/AbstractCommand";
 import Phoenix from "../../Phoenix";
+import { MessageEmbed } from "discord.js";
 
 export default class ReloadCommand extends AbstractCommand{
     constructor() {
@@ -7,17 +8,95 @@ export default class ReloadCommand extends AbstractCommand{
             name: 'reload',
             onlyOwner: true,
             category: 'owner',
-            subCommands: [{methodName: 'textController'}]
+            subCommands: [
+                {methodName: 'commandController',methodAliases: ['command', 'commands']},
+                {methodName: 'eventController',methodAliases: ['event', 'events']},
+                {methodName: 'phoenixUserController',methodAliases: ['pusers', 'phoenixuser', 'phoenixusers']},
+                {methodName: 'serverController',methodAliases: ['server', 'servers']},
+                {methodName: 'textController',methodAliases: ['text', 'texts']},
+            ]
         });
     }
 
-    public run({ }) {
+    public commandController({ message }: ICommandParameters) {
+        let destroyTime = Date.now();
+        Phoenix.getCommandController().destroy();
+        destroyTime = Date.now() - destroyTime;
+        let initTime = Date.now();
+        Phoenix.getCommandController().init();
+        initTime = Date.now() - initTime;
+        return message.channel.send(new MessageEmbed().setDescription(`Sucess, destroy in ${destroyTime}ms and init in ${initTime}ms`));
+    }
+    public eventController({ message }: ICommandParameters) {
+        let destroyTime = Date.now();
+        Phoenix.getEventController().destroy();
+        destroyTime = Date.now() - destroyTime;
+        let initTime = Date.now();
+        Phoenix.getEventController().init();
+        initTime = Date.now() - initTime;
+        return message.channel.send(new MessageEmbed().setDescription(`Sucess, destroy in ${destroyTime}ms and init in ${initTime}ms`));
+        
+    }
+    public phoenixUserController({ message }: ICommandParameters) {
+        let destroyTime = Date.now();
+        Phoenix.getPhoenixUserController().destroy();
+        destroyTime = Date.now() - destroyTime;
+        let initTime = Date.now();
+        Phoenix.getPhoenixUserController().init();
+        initTime = Date.now() - initTime;
+        return message.channel.send(new MessageEmbed().setDescription(`Sucess, destroy in ${destroyTime}ms and init in ${initTime}ms`));
+        
+    }
+    public serverController({ message }: ICommandParameters) {
+        let destroyTime = Date.now();
+        Phoenix.getServerController().destroy();
+        destroyTime = Date.now() - destroyTime;
+        let initTime = Date.now();
+        Phoenix.getServerController().init();
+        initTime = Date.now() - initTime;
+        return message.channel.send(new MessageEmbed().setDescription(`Sucess, destroy in ${destroyTime}ms and init in ${initTime}ms`));
+        
+    }
+    public textController({ message }: ICommandParameters) {
+        let destroyTime = Date.now();
+        Phoenix.getTextController().destroy();
+        destroyTime = Date.now() - destroyTime;
+        let initTime = Date.now();
+        Phoenix.getTextController().init();
+        initTime = Date.now() - initTime;
+        return message.channel.send(new MessageEmbed().setDescription(`Sucess, destroy in ${destroyTime}ms and init in ${initTime}ms`));
         
     }
 
-    public async textController({ message }: ICommandParameters) {
+    public run({ message}:ICommandParameters) {
+        let destroyMessage = '';
+        let initMessage = '';
+        let time = Date.now();
+        Phoenix.getCommandController().destroy();
+        destroyMessage += `CommandController ${Date.now() - time}ms\n`;
+        time = Date.now();
+        Phoenix.getPhoenixUserController().destroy();
+        destroyMessage += `PhoenixUserController ${Date.now() - time}ms\n`;
+        time = Date.now();
+        Phoenix.getServerController().destroy();
+        destroyMessage += `ServerController ${Date.now() - time}ms\n`;
+        time = Date.now();
         Phoenix.getTextController().destroy();
+        destroyMessage += `TextController ${Date.now() - time}ms\n`;
+        time = Date.now();
+        
+        Phoenix.getCommandController().init();
+        initMessage += `TextController ${Date.now() - time}ms\n`;
+        time = Date.now();
+        Phoenix.getPhoenixUserController().init();
+        initMessage += `PhoenixUserController ${Date.now() - time}ms\n`;
+        time = Date.now();
+        Phoenix.getServerController().init();
+        initMessage += `ServerController ${Date.now() - time}ms\n`;
+        time = Date.now();
         Phoenix.getTextController().init();
-        return message.reply('done');
+        initMessage += `TextController ${Date.now() - time}ms\n`;
+        time = Date.now();
+        return message.channel.send(new MessageEmbed().addField(`Destroy`, destroyMessage, true).addField('Init', initMessage, true));
     }
 }
